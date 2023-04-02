1f3861fbee19148adab0aac4e3ae72e4279dee7c
bamboo <minnie20000622@yahoo.co.jp>
2023-04-02 20:25:54 +0900

    aboutページの作成

---
+++ my-portfolio/src/components/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Article = () => {
+  return (
+    <div>
+      <h2>about</h2>
+      <p>竹澤彩里です！</p>
+      <img src='./logo192.png'></img>
+      <p>
+        2023年4月からエンジニアとして働いています。<br/>
+        2019年6月ごろからプログラミングを始めました。Pythonで競技プログラミングを中心に、2022年ごろからは各種ハッカソンに参加していました。<br/>
+        大学では、音響学を専攻してました。
+      </p>
+      <p>
+        この辺に職歴とか人生の歴史書く
+      </p>
+    </div>
+  );
+};
+
+export default Article;
