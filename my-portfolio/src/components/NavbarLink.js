5640f7c9e7e3bd539c237c568eadeb2d513c3801
bamboo <minnie20000622@yahoo.co.jp>
2023-04-02 21:25:29 +0900

    Navbarのデザインをいい感じにした

--- my-portfolio/src/components/NavbarLink.js
+++ my-portfolio/src/components/NavbarLink.js
@@ -1,9 +1,16 @@
+import { fontFamily } from '@mui/system';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NavbarLink = ({href, children}) => {
   return (
-    <Link to={href} style={{ textDecoration: 'none' }}>
+    <Link to={href} style={{
+      textDecoration: 'none',
+      fontSize: '16px',
+      color: '#757575',
+      fontWeight: 'bold',
+      fontFamily: '"Cormorant Garamond", serif'
+      }}>
       {children}
     </Link>
   )
