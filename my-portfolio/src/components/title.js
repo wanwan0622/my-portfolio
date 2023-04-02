94cef5fdae1d00eb874eb4d02bea1c125722cd3c
bamboo <minnie20000622@yahoo.co.jp>
2023-04-02 20:29:06 +0900

    titleページの作成

---
+++ my-portfolio/src/components/title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/title.module.css'
+
+const Article = () => {
+  return (
+    <div>
+      <h2 className={styles.subtitle}>Ayari Takezawa's</h2>
+      <h1 className={styles.title}>PORTFOLIO</h1>
+      <p className={styles.details}>竹澤彩里のポートフォリオです♡</p>
+    </div>
+  );
+};
+
+export default Article;
