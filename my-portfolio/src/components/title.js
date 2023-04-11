1673c7c947aeafc0ef0cf71e6648aece59414c76
bamboo <minnie20000622@yahoo.co.jp>
2023-04-11 22:20:56 +0900

    タイトルの余白の調整

--- my-portfolio/src/components/title.js
+++ my-portfolio/src/components/title.js
@@ -3,7 +3,7 @@ import styles from '../styles/Title.module.css'
 
 const Title = () => {
   return (
-    <div>
+    <div className={styles.title_container}>
       <h2 className={styles.subtitle}>Ayari Takezawa's</h2>
       <h1 className={styles.title}>PORTFOLIO</h1>
       <p className={styles.details}>竹澤彩里のポートフォリオです♡</p>
