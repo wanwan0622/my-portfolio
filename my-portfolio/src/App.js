7b049e5b91d7b6fc1dc3c34c259bccd9b8db4e92
bamboo <minnie20000622@yahoo.co.jp>
2023-04-17 21:34:45 +0900

    キラキラのマウスストーカーを追加

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Title from './components/Title.js';
 import Navbar from './components/Navbar.js';
 import Feature from './components/Feature.js';
 import About from './components/About.js';
 import Skills from './components/Skills.js';
 import Contact from './components/Contact.js';
+import MouseStalker from './components/MouseStalker';
 
 function App() {
   return (
     <div className="App">
+      <MouseStalker />
       <Title />
       <Navbar />
       <Feature />
