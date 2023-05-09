d09825d2d886819f6d34fec9d8431d8ae199317d
bamboo <minnie20000622@yahoo.co.jp>
2023-05-09 22:33:17 +0900

    AboutページのUIを整えた

--- my-portfolio/src/components/about.js
+++ my-portfolio/src/components/about.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
+import styles from '../styles/About.module.css'
 
 const About = () => {
   return (
     <div>
-      <h2 className='heading-primary'>about</h2>
+      <h2 className="heading-primary">about</h2>
       <p>竹澤彩里です！</p>
-      <img src='./logo192.png'></img>
-      <p>
-        2023年4月からエンジニアとして働いています。<br/>
-        2019年6月ごろからプログラミングを始めました。Pythonで競技プログラミングを中心に、2022年ごろからは各種ハッカソンに参加していました。<br/>
-        大学では、音響学を専攻してました。
-      </p>
-      <p>
-        この辺に職歴とか人生の歴史書く
-      </p>
+      <div className={styles.column}>
+        <img src="./icon.png" width={"20%"}></img>
+        <div>
+          <p>
+            2023年4月からエンジニアとして働いています。
+            <br />
+            2019年6月ごろからプログラミングを始めました。Pythonで競技プログラミングを中心に、2022年ごろからは各種ハッカソンに参加していました。
+            <br />
+            大学では、音響学を専攻してました。
+          </p>
+          <p>この辺に職歴とか人生の歴史書く</p>
+        </div>
+      </div>
     </div>
   );
 };
